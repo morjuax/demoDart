29995dc65e10b0659f5c21061cd0b06cf2cfce6d
Juan Moreno <morjua19@gmail.com>
2021-01-16 12:52:19 -0300

    refresh gitignore

--- bin/demo.dart
+++ bin/demo.dart
@@ -1,10 +1,21 @@
 import 'package:demo/demo.dart' as demo;
 
 void main(List<String> arguments) {
-  String var1 = 'Hello World';
-  String var2 = 'd';
-  String var3 = var1??var2;
-  
-  print(var3);
+  // String var1 = 'Hello World';
+  // String var2 = 'd';
+  // String var3 = var1??var2;
+
+  // print(var3);
   //print('${var1}: ${demo.calculate()}!!!');
+
+  // print(5/2);
+  // print(5/2);
+
+  // En otros lenguajes se conoce arreglos
+  // List<String> list = ['1','2','3','4','5'];
+  // print(list);
+
+  Map<String, int> map = { 'name': 1, 'lastName': 2};
+
+  print(map);
 }
