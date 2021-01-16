c302711059a6f6e78e4f3d02f19e76df8ab7e0d5
Juan Moreno <morjua19@gmail.com>
2021-01-16 11:40:12 -0300

    INIT project

---
+++ bin/demo.dart
@@ -0,0 +1,10 @@
+import 'package:demo/demo.dart' as demo;
+
+void main(List<String> arguments) {
+  String var1 = 'Hello World';
+  String var2 = 'd';
+  String var3 = var1??var2;
+  
+  print(var3);
+  //print('${var1}: ${demo.calculate()}!!!');
+}
