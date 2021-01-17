7423763b538f17dbd786d2aaee35736bb9faa6a4
Juan Moreno <morjua19@gmail.com>
2021-01-16 21:19:30 -0300

    [ADD] demo classes

---
+++ bin/classDemo.dart
@@ -0,0 +1,20 @@
+void main() {
+  // Point point = Point(x:2.0, y:1.0);
+  Point point = Point.mitad(z: 15.0);
+  print(point.toString2());
+}
+
+class Point {
+  final double x;
+  final double y;
+  final double z;
+
+  Point({this.x=5.0, this.y, this.z=3.0}) {}
+
+  Point.mitad({this.z}):this.x=z/2,this.y=z/2;
+
+  String toString2() {
+    // this.z = x + y;
+    return "${this.x}";
+  }
+}
