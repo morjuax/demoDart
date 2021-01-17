cca3a08d77d06a091438f51632926bf2321bfd0b
Juan Moreno <morjua19@gmail.com>
2021-01-17 13:26:36 -0300

    [UPD] group test

--- test/circle_test.dart
+++ test/circle_test.dart
@@ -6,12 +6,26 @@ void main() {
   setUp(() {
     circle = Circle(radio: 5);
   });
+  
+  group('Test positive circle', () {
+    test('Calculating area of a circle', () {
+      expect(circle.area(), equals(78.53750000000001));
+    });
 
-  test('Calculating area of a circle', () {
-    expect(circle.area(), 78.53750000000001); // 5.0*5.0*3.1415);
+    test('Calculating perimeter of a circle', () {
+      expect(circle.perimeter(), equals(10.1415));
+    });
   });
 
-  test('Calculating perimeter of a circle', () {
-    expect(circle.perimeter(), 10.1415);
+  group('Test negative circle', () {
+    test('area must be nonzero', () {
+      expect(circle.area(), isNot(0));
+    });
+
+    test('perimeter must be nonzero', () {
+      expect(circle.perimeter(), isNot(0));
+    });
   });
+
+
 }
